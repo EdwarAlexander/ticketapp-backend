9f6a6fb616281d17e858b1e9ee782884b7d7ea2c
edwarmoran <edwar.moran.12@gmail.com>
2021-01-27 21:18:32 -0500

    creacion de proyecto

---
+++ src/main/java/com/eamr/TicketappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eamr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eamr/TicketappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eamr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
